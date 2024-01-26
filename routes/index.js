ea692ea5aa4f39ace408036b19ea7ce64c664825
Priyanshu Singh <priyanshusinghajm@gmail.com>
2024-01-26 10:14:31 +0530

    profile page updated

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const userModel = require('./users')
 const postModel = require('./posts')
-
 const passport = require('passport');
+const upload = require('./multer')
 
 const localStrategy = require('passport-local');
 passport.use(new localStrategy(userModel.authenticate()))
@@ -13,15 +13,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res) {
-  res.render('login');
+  res.render('login', {error: req.flash('error')});
 })
 
 router.get('/feed', function(req, res) {
   res.render('feed');
 })
 
-router.get('/profile', isLoggedIn, function(req, res, next) {
-  res.render('profile');
+router.post('/upload', isLoggedIn, upload.single('file'), async function(req, res, next) {
+  if(!req.file){
+    return res.status(404).send("No files were uploaded")
+  }
+  const user = await userModel.findOne({username: req.session.passport.user});
+
+  const post = await postModel.create({
+    image: req.file.filename,
+    imageText: req.body.filecaption,
+    user: user._id
+  })
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect('/profile');
+})
+
+router.get('/profile', isLoggedIn, async function(req, res, next) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  .populate('posts')
+  res.render('profile', {user});
 })
 
 router.post('/register', function(req, res) {
@@ -42,7 +63,8 @@ router.post('/register', function(req, res) {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/profile',
-  failureRedirect: '/login'
+  failureRedirect: '/login',
+  failureFlash: true
 }), function(req, res) {})
 
 router.get('/logout', function(req, res) {
